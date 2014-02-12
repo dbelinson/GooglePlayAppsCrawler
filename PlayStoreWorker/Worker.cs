64f0aaba84cb8e1316f9d9a9cc06f284e59b1223
Marcello Lins <marcello.grechi@gmail.com>
2014-02-12 21:48:58 -0200

    Better exception treatment

--- PlayStoreWorker/Worker.cs
+++ PlayStoreWorker/Worker.cs
@@ -41,86 +41,93 @@ namespace PlayStoreWorker
                 // Iterating Over MongoDB Records while no document is found to be processed                
                 while ((app = mongoDB.FindAndModify ()) != null)
                 {
-                    // Building APP URL
-                    string appUrl = Consts.APP_URL_PREFIX + app.Url;
-
-                    // Checking if this app is on the database already
-                    if (mongoDB.AppProcessed (appUrl))
+                    try
                     {
-                        // Console Feedback, Comment this line to disable if you want to
-                        Console.WriteLine ("Duplicated App, skipped.");
+                        // Building APP URL
+                        string appUrl = Consts.APP_URL_PREFIX + app.Url;
 
-                        // Delete it from the queue and continues the loop
-                        mongoDB.RemoveFromQueue(appUrl);
-                        continue;
-                    }
-                    
-                    // Configuring server and Issuing Request
-                    server.Headers.Add (Consts.ACCEPT_LANGUAGE);
-                    server.Host = Consts.HOST;
-                    server.Encoding = "utf-8";
-                    server.EncodingDetection = WebRequests.CharsetDetection.DefaultCharset;
-                    string response = server.Get (appUrl);
-
-                    // Flag Indicating Success while processing and parsing this app
-                    bool ProcessingWorked = true;
-
-                    // Sanity Check
-                    if (String.IsNullOrEmpty (response) || server.StatusCode != System.Net.HttpStatusCode.OK)
-                    {
-                        LogWriter.Info ("Error opening app page : " + appUrl);
-                        ProcessingWorked = false;
-                    }
-                    else
-                    {
-                        // Parsing Useful App Data
-                        AppModel parsedApp = parser.ParseAppPage (response, appUrl);
-
-                        // Inserting App into Mongo DB Database
-                        if (!mongoDB.Insert<AppModel> (parsedApp))
-                        {
-                            ProcessingWorked = false;
-                        }
-
-                        // If the processing failed, do not remove the app from the database, instead, keep it and flag it as not busy 
-                        // so that other workers can try to process it later
-                        if (!ProcessingWorked)
-                        {
-                            mongoDB.ToggleBusyApp (app, false);
-                        }
-                        else // On the other hand, if processing worked, removes it from the database
+                        // Checking if this app is on the database already
+                        if (mongoDB.AppProcessed(appUrl))
                         {
                             // Console Feedback, Comment this line to disable if you want to
-                            Console.WriteLine ("Inserted App : " + parsedApp.Name);
-                             
-                            mongoDB.RemoveFromQueue (app.Url);
+                            Console.WriteLine("Duplicated App, skipped.");
+
+                            // Delete it from the queue and continues the loop
+                            mongoDB.RemoveFromQueue(appUrl);
+                            continue;
                         }
 
-                        // Counters for console feedback only
-                        int extraAppsCounter = 0, newExtraApps = 0;
+                        // Configuring server and Issuing Request
+                        server.Headers.Add(Consts.ACCEPT_LANGUAGE);
+                        server.Host = Consts.HOST;
+                        server.Encoding = "utf-8";
+                        server.EncodingDetection = WebRequests.CharsetDetection.DefaultCharset;
+                        string response = server.Get(appUrl);
 
-                        // Parsing "Related Apps" and "More From Developer" Apps (URLS Only)
-                        foreach (string extraAppUrl in parser.ParseExtraApps (response))
+                        // Flag Indicating Success while processing and parsing this app
+                        bool ProcessingWorked = true;
+
+                        // Sanity Check
+                        if (String.IsNullOrEmpty(response) || server.StatusCode != System.Net.HttpStatusCode.OK)
                         {
-                            // Incrementing counter of extra apps
-                            extraAppsCounter++;
-
-                            // Assembling Full app Url to check with database
-                            string fullExtraAppUrl = Consts.APP_URL_PREFIX + extraAppUrl;
-
-                            // Checking if the app was either processed or queued to be processed already
-                            if ( (!mongoDB.AppProcessed (fullExtraAppUrl)) && (!mongoDB.IsAppOnQueue (extraAppUrl)))
-                            {
-                                // Incrementing counter of inserted apps
-                                newExtraApps++;
-
-                                // Adds it to the queue of apps to be processed
-                                mongoDB.AddToQueue (extraAppUrl);
-                            }
+                            LogWriter.Info("Error opening app page : " + appUrl);
+                            ProcessingWorked = false;
                         }
+                        else
+                        {
+                            // Parsing Useful App Data
+                            AppModel parsedApp = parser.ParseAppPage(response, appUrl);
 
-                        // Console Feedback
-                        Console.WriteLine ("Queued " + newExtraApps + " / " + extraAppsCounter + " related apps");
+                            // Inserting App into Mongo DB Database
+                            if (!mongoDB.Insert<AppModel>(parsedApp))
+                            {
+                                ProcessingWorked = false;
+                            }
+
+                            // If the processing failed, do not remove the app from the database, instead, keep it and flag it as not busy 
+                            // so that other workers can try to process it later
+                            if (!ProcessingWorked)
+                            {
+                                mongoDB.ToggleBusyApp(app, false);
+                            }
+                            else // On the other hand, if processing worked, removes it from the database
+                            {
+                                // Console Feedback, Comment this line to disable if you want to
+                                Console.WriteLine("Inserted App : " + parsedApp.Name);
+
+                                mongoDB.RemoveFromQueue(app.Url);
+                            }
+
+                            // Counters for console feedback only
+                            int extraAppsCounter = 0, newExtraApps = 0;
+
+                            // Parsing "Related Apps" and "More From Developer" Apps (URLS Only)
+                            foreach (string extraAppUrl in parser.ParseExtraApps(response))
+                            {
+                                // Incrementing counter of extra apps
+                                extraAppsCounter++;
+
+                                // Assembling Full app Url to check with database
+                                string fullExtraAppUrl = Consts.APP_URL_PREFIX + extraAppUrl;
+
+                                // Checking if the app was either processed or queued to be processed already
+                                if ((!mongoDB.AppProcessed(fullExtraAppUrl)) && (!mongoDB.IsAppOnQueue(extraAppUrl)))
+                                {
+                                    // Incrementing counter of inserted apps
+                                    newExtraApps++;
+
+                                    // Adds it to the queue of apps to be processed
+                                    mongoDB.AddToQueue(extraAppUrl);
+                                }
+                            }
+
+                            // Console Feedback
+                            Console.WriteLine("Queued " + newExtraApps + " / " + extraAppsCounter + " related apps");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        LogWriter.Error (ex);
                     }
                 }
             }
